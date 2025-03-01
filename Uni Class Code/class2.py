6a01bfbdfb38ca2fbc2e99f4ea9b0322ed46160c
Suheer Khan <khansuheer1@gmail.com>
2025-03-01 17:00:16 +0500

    Solved Another

--- Uni Class Code/class2.py
+++ Uni Class Code/class2.py
@@ -1,3 +1,4 @@
+import math
 result = 0
 for i in range(0,10):
     for j in range (0, i):
@@ -13,3 +14,17 @@ for i in range(0, 32):
         num = num - 1
     print(fact)
     
+str = "suheer"
+countA = 0
+countE = 0
+countI = 0
+countO = 0
+countU = 0
+count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for i in range(0, len(str)-1):
+    count[int(ord(str[i]))-97] += 1
+    
+for i in range(0, len(str)):
+    print(count[int(ord(str[i]))-97])
+
+
