9b479e4099bc1137ebbfeff76d98a2e3f3b9b4cd
Suheer Khan <khansuheer1@gmail.com>
2025-03-01 17:00:29 +0500

    Solved Another

--- Uni Class Code/class2.py
+++ Uni Class Code/class2.py
@@ -14,6 +14,8 @@ for i in range(0, 32):
         num = num - 1
     print(fact)
     
+
+#CHECK NUMBER OF VOWELS WITHOUT IF STATEMENT
 str = "suheer"
 countA = 0
 countE = 0
@@ -23,8 +25,11 @@ countU = 0
 count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 for i in range(0, len(str)-1):
     count[int(ord(str[i]))-97] += 1
-    
-for i in range(0, len(str)):
-    print(count[int(ord(str[i]))-97])
+
+print(count[0])
+print(count[4])
+print(count[8])
+print(count[14])
+print(count[20])
 
 
